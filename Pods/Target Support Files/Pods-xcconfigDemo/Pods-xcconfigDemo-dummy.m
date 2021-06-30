05e5a220399859a4b1abd773bc6a648eff9bf30a
zq <869518570@qq.com>
2021-06-30 16:48:28 +0800

    Initial commit

---
+++ Pods/Target Support Files/Pods-xcconfigDemo/Pods-xcconfigDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xcconfigDemo : NSObject
+@end
+@implementation PodsDummy_Pods_xcconfigDemo
+@end
---
+++ xcconfigDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  xcconfigDemo
+//
+//  Created by tcxt on 2021/4/6.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
